67079ca626eb91c76b77565332172fad808a3208
KatyTaylor <katytaylor57@gmail.com>
2019-08-12 12:50:35 +0100

    tests for checkParams

--- src/mainTest.java
+++ src/mainTest.java
@@ -5,6 +5,60 @@ import org.junit.jupiter.api.Test;
 class mainTest {
 
 	@Test
+	void testCheckParams_negative_nullParams() {
+		Integer expectedNumberParams = 1;
+		String[] params = null;
+		
+		Boolean correctParams = main.checkParams(expectedNumberParams, params);		
+		assert(!correctParams);		
+	}
+	
+	@Test
+	void testCheckParams_negative_nullFirstParam() {
+		Integer expectedNumberParams = 1;
+		String[] params = {null};
+		
+		Boolean correctParams = main.checkParams(expectedNumberParams, params);		
+		assert(!correctParams);		
+	}
+	
+	@Test
+	void testCheckParams_negative_notEnoughParams() {
+		Integer expectedNumberParams = 3;
+		String[] params = {"param1", "param2"};
+		
+		Boolean correctParams = main.checkParams(expectedNumberParams, params);		
+		assert(!correctParams);		
+	}
+	
+	@Test
+	void testCheckParams_positive_1params() {
+		Integer expectedNumberParams = 1;
+		String[] params = {"param1"};
+		
+		Boolean correctParams = main.checkParams(expectedNumberParams, params);		
+		assert(correctParams);		
+	}
+	
+	@Test
+	void testCheckParams_positive_manyParams() {
+		Integer expectedNumberParams = 3;
+		String[] params = {"param1", "param2", "param3"};
+		
+		Boolean correctParams = main.checkParams(expectedNumberParams, params);		
+		assert(correctParams);		
+	}
+	
+	@Test
+	void testCheckParams_positive_excessParams() {
+		Integer expectedNumberParams = 3;
+		String[] params = {"param1", "param2", "param3", "param4"};
+		
+		Boolean correctParams = main.checkParams(expectedNumberParams, params);		
+		assert(correctParams);		
+	}
+	
+	@Test
 	void testValidateuserInput() {
 		String userInput = "command";
 		Boolean success = main.validateUserInput(userInput);		
